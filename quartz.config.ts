e24220663e55ce407a4950a2be384f4f8d14e33a
PSH <201820916@github.com>
2026-04-09 11:10:29 +0900

    Quartz sync: Apr 9, 2026, 11:10 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "201820916.github.io/my-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
